ca2f51f79933be52465551c5990707754bd8e37c
Josh Elias <josh@daemontech.tools>
2023-11-04 12:50:28 -0400

    add tailwind to RazorUI

---
+++ RazorUI/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.{razor,html,cshtml}",
+  ],
+  output: "./wwwroot/css/bundle.css",
+  theme: {
+    fontFamily: {
+        'body': ['Nunito', 'system-ui', 'sans-serif']
+    },
+    container: {
+        center: true,
+        padding: {
+            DEFAULT: '1rem',
+            sm: '2rem',
+            lg: '4rem',
+            xl: '5rem',
+            '2xl': '6rem',
+        },
+    }
+  },
+  plugins: [
+      require('@tailwindcss/forms')
+  ],
+}
