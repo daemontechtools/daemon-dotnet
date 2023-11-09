70d74b0c02ecb2a74b516acd2abc9a9f0d85564c
Josh Elias <josh@daemontech.tools>
2023-11-09 11:10:23 -0500

    change IconProps Don't dynamically generate tailwind classes. The tailwind transpiler won't be able to detect them and generate the appropriate css.

--- RazorUI/Icons/IconProps.cs
+++ RazorUI/Icons/IconProps.cs
@@ -4,14 +4,5 @@ namespace Daemon.RazorUI.Icons;
 
 
 public struct IconProps {
-    public TailwindColor? Color { get; set; }
-    public TailwindColor? HoverColor { get; set; }
-    public string? Transition { get; set; }
-
-    public IconProps() {}
-
-    public string ToClass() 
-    {
-        return $"{(Color != null ? $"stroke-{Color?.Name}-{Color?.Intensity}" : string.Empty)} {(HoverColor != null ? $"hover:stroke-{HoverColor?.Name}-{HoverColor?.Intensity}" : string.Empty)} {(Transition != null ? Transition : string.Empty)}".Trim();
-    }
+    public string Class { get; set; }
 }
